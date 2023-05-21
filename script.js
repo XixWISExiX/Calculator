aadc9c84e3fc06886ba4148ad2603ffb2b349bed
XixWISExiX <addalooptoit@gmail.com>
2023-05-20 22:30:43 -0500

    Able to Obtain Number from Button Press

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+import { body } from "./DOMref.js";
+
 function application() {
+  //   let display = document.getElementsByClassName("display")[0].text;
+  //   console.log(display);
+  const buttons = body.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", buttonSelection);
+  });
+  operate(calculator.num1, calculator.operator, calculator.num2);
+  //   console.log(operate(calculator.num1, calculator.operator, calculator.num2));
+}
+
+let calculator = (function () {
   let num1 = 6;
   let operator = "+";
   let num2 = 3;
-  operate(num1, operator, num2);
+  return { num1, operator, num2 };
+})();
+
+function buttonSelection() {
+  let partNum1 = this.textContent;
+  console.log(partNum1);
 }
 
 function operate(num1, operator, num2) {
@@ -28,4 +46,4 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-application();
+document.addEventListener("DOMContentLoaded", application);
