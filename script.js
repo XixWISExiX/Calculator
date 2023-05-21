f16765c780ceaf0fd3c0b225d079bf605c434918
XixWISExiX <addalooptoit@gmail.com>
2023-05-20 21:13:36 -0500

    Added Fase Fogic Functions

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function add() {}
+function application() {
+  let num1 = 6;
+  let operator = "+";
+  let num2 = 3;
+  operate(num1, operator, num2);
+}
 
-function subtract() {}
+function operate(num1, operator, num2) {
+  if (operator === "+") return add(num1, num2);
+  if (operator === "-") return subtract(num1, num2);
+  if (operator === "*") return multiply(num1, num2);
+  if (operator === "/") return divide(num1, num2);
+}
 
-function multiply() {}
+function add(num1, num2) {
+  return num1 + num2;
+}
 
-function divide() {}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+application();
